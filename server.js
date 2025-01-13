be33f3c53d0831024d25913f20fbf882a1fecb30
jonathan <johh.almeida@gmail.com>
2025-01-13 20:31:37 -0300

    fixed-layout

--- server.js
+++ server.js
@@ -9,34 +9,39 @@ const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
-const static = require("./routes/static")
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
-
-// Index Route
-app.get("/", function(req,res){
-  res.render("index",{title:"Home"})
-})
 
 /* ***********************
  * EJS
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("layout", "./layouts/layout") // layout.ejs na pasta layouts
+
+// Configuração para arquivos estáticos (CSS, JS, etc.)
+app.use(express.static("public"));
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+
+app.use(require("./routes/static"));
+
+// Index Route
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" });
+});
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`App listening on http://${host}:${port}`);
+});
