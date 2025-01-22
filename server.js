6768f5f22e4a714dfd9ac828fe97a1b498108512
jonathan <johh.almeida@gmail.com>
2025-01-22 20:39:41 -0300

    week3activities

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+
 
 /* ***********************
  * Routes
@@ -23,7 +26,19 @@ app.set("layout", "./layouts/layout") // layout.ejs na pasta layouts
 
 // Configuração para arquivos estáticos (CSS, JS, etc.)
 app.use(express.static("public"));
-
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -32,9 +47,15 @@ app.use(express.static("public"));
 app.use(require("./routes/static"));
 
 // Index Route
-app.get("/", function(req, res) {
-  res.render("index", { title: "Home" });
-});
+//app.get("/", function(req, res) {
+  //res.render("index", { title: "Home" });
+//});
+app.get("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
 
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
